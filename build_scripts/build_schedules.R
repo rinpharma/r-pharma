1bbfaf968c432b98dd659feade04ca237e462218
James Black <james@epijim.uk>
2021-03-24 22:54:01 +0100

    tidy up build schedules to handle dates better

--- build_scripts/build_schedules.R
+++ build_scripts/build_schedules.R
@@ -9,11 +9,16 @@ sheet_url <- "https://docs.google.com/spreadsheets/d/1NaDnMRh2nOBCzBUxbIyJBVWd_I
 # check the value of the option, if you like
 options(gargle_oauth_email = "james.black.jb2@roche.com")
 gs4_auth(email = "james.black.jb2@roche.com", cache = ".secrets")
-d_raw_proceedings <- read_sheet(sheet_url, sheet = "all_conferences")
+d_raw_proceedings <- read_sheet(
+  sheet_url, 
+  sheet = "all_conferences",
+  col_types = "ccccDcccccc"
+  )
 
 # Clean data -------------------------------------------------------------------
 
 d_all <- d_raw_proceedings %>%
+  
   arrange(Date,Start) %>%
   mutate(
     # Modify col
@@ -23,12 +28,16 @@ d_all <- d_raw_proceedings %>%
     ID = paste0("rinpharma_",row_number())
   ) %>%
   mutate(
-    StartTime = strftime(Start, format="%H:%M:%S"),
-    EndTime = strftime(End, format="%H:%M:%S")
+    StartTime = anytime::anytime(paste(Date, Start)),
+    EndTime = anytime::anytime(paste(Date, End)),
+    StartTime = strftime(StartTime, format="%l:%M %p"),
+    EndTime = strftime(EndTime, format="%l:%M %p"),
+    
+    EndTime = ifelse(EndTime == "12:00 am",NA,EndTime)
   ) %>%
   select(
     ID,Event,Abstract,Type, Year,Date, Speaker, Affiliation, Title, Slides, Video,
-    StartTime,EndTime
+    StartTime,EndTime,Start,End
   )
 
 ## build website schedule yml files 
@@ -39,27 +48,27 @@ meta <- list(
     data = d_all %>%
       filter(Event == "2018 Conference"),
     #days = c(1,2),
-    url = "rinpharma2018",
+    url = "rinpharma2018"
     #titles = c("Day 1", "Day 2"),
-    subtitles = c("15 Aug [9:15am - 5:25pm ET]", "16 Aug [9:15am - 5:25pm ET]")
+    #subtitles = c("15 Aug [9:15am - 5:25pm ET]", "16 Aug [9:15am - 5:25pm ET]")
   ),
   schedule2019 = list(
     year = 2019,
     data = d_all %>%
       filter(Event == "2019 Conference"),
     #days = c(1,2,3),
-    url = "rinpharma2018",
-    #titles = c("Day 1 - Workshops", "Day 2", "Day 3"),
-    subtitles = c("20 Aug [8:00am - 5:20pm ET]", "21 Aug [8:15am - 5:35pm ET]", "22 Aug [8:15am - 5:45pm ET]")
+    url = "rinpharma2018"
+    #titles = c("Day 1 - Workshops", "Day 2", "Day 3")
+    #subtitles = c("20 Aug [8:00am - 5:20pm ET]", "21 Aug [8:15am - 5:35pm ET]", "22 Aug [8:15am - 5:45pm ET]")
   ),
   schedule2020 = list(
     year = 2020,
     data = d_all %>%
       filter(Event == "2020 Conference"),
     #days = c(1,2,3,4),
-    url = "rinpharma2018",
+    url = "rinpharma2018"
     #titles = c("Workshops", "Day 1", "Day 2", "Day 3"),
-    subtitles = c("5 Oct - 9 Oct", "13 Oct [10:00am - 2:10 pm ET]", "14 Oct [10:00am - 2:20 pm ET]", "15 Oct [10:00am - 2:10 pm ET]")
+    #subtitles = c("5 Oct - 9 Oct", "13 Oct [10:00am - 2:10 pm ET]", "14 Oct [10:00am - 2:20 pm ET]", "15 Oct [10:00am - 2:10 pm ET]")
   )
 )
 
@@ -76,7 +85,7 @@ for (yr in seq_along(meta)) {
   enable : true
   titleOutline : "schedule"
   title : "R/Pharma {meta[[yr]]$year}"
-  content : "All times below in US ET. See proceedings for final abstracts."
+  content : "All times below in US ET."
   scheduleTab:', .trim = FALSE, .na = ""
   ))
 
@@ -85,11 +94,21 @@ for (yr in seq_along(meta)) {
     
     df_day_range <- glue(
       "{min_start} to {min_end}",
-      min_start = min(df_day$StartTime, na.rm = TRUE),
+      min_start = df_day %>%
+        arrange(Start) %>%
+        slice(1) %>% pull(StartTime),
       min_end = max(
         c(
-          max(df_day$EndTime, na.rm = TRUE),
-          max(df_day$StartTime, na.rm = TRUE)
+          {
+            df_day %>%
+              arrange(desc(Start)) %>%
+              slice(1) %>% pull(StartTime)
+          },
+          {
+            df_day %>%
+              arrange(desc(End)) %>%
+              slice(1) %>% pull(EndTime)
+          }
         ), na.rm = TRUE
       )
     )
@@ -97,7 +116,7 @@ for (yr in seq_along(meta)) {
     cat(glue(
       '
     ################################# tab itam loop
-    - title : "{as.character(day_val)}"
+    - title : "{strftime(as.Date(day_val, origin = "1970-01-01"), format="%d %b")}"
       subtitle : "{df_day_range}"
       tabContentList:', .trim = FALSE, .na = ""
     ))
